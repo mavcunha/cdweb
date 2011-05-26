49efac517137e0c35720b0686b4b8e5d61606194
Marco Valtas <mavcunha@gmail.com>
2011-05-26 02:16:59 -0300

    More tests, order is important (but, can we test for FileFinder?)

--- test/fs/FilesTest.java
+++ test/fs/FilesTest.java
@@ -1,6 +1,7 @@
 package fs;
 
 import org.junit.Test;
+import org.mockito.internal.util.reflection.FieldSetter;
 import play.test.UnitTest;
 
 import java.io.File;
@@ -8,6 +9,7 @@ import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 
 public class FilesTest extends UnitTest {
 
@@ -29,5 +31,30 @@ public class FilesTest extends UnitTest {
         assertThat(iterator.hasNext(), is(false));
     }
 
+    @Test
+    public void shouldRetrieveInSameOrder() {
+        File file1 = mock(File.class);
+        File file2 = mock(File.class);
 
+        Files files = new Files();
+        files.add(file1);
+        files.add(file2);
+
+        Iterator<File> iterator = files.iterator();
+        assertThat(iterator.next(),is(file1));
+        assertThat(iterator.next(),is(file2));
+    }
+
+    @Test
+    public void emptyFilesObjecReturnsIteratorWithoutItems() {
+        Files files = new Files();
+        assertThat(files.iterator().hasNext(),is(false));
+    }
+
+    @Test
+    public void iteratorNotEmptyForNonEmptyFilesObject() {
+        Files files = new Files();
+        files.add(new File("some_file"));
+        assertThat(files.iterator().hasNext(),is(true));
+    }
 }
