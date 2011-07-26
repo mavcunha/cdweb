5f71872e373525ad8f26b0390439c4bcb2db5639
Marco Valtas <mavcunha@gmail.com>
2011-07-26 03:00:53 -0300

    Fixed static update message error.
    Added to Application controller a read method to read the update
    message every time the page is accessed, dropped the use of template
    include.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,13 @@ import fs.Files;
 import models.User;
 import play.Play;
 import play.mvc.Before;
+import play.mvc.Catch;
 import play.mvc.Controller;
 import play.mvc.With;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 @With(Secure.class)
@@ -28,7 +31,21 @@ public class Application extends Controller {
     public static void index() {
         Files files = new FileFinder(pathToFiles).find(Pattern.compile(".*\\.rtf$"));
         renderArgs.put("filesRootPath", filesRootPath);
+        renderArgs.put("updateInfo",updateInfo());
         render(files);
+    }
+
+    private static String updateInfo()  {
+        StringBuilder content = new StringBuilder();
+        try {
+            Scanner scanner = new Scanner(new File("public/data/update.html"));
+            while (scanner.hasNextLine())
+                content.append(scanner.nextLine());
+
+        } catch (FileNotFoundException e) {
+
+        }
+        return content.toString();
     }
 
 }
